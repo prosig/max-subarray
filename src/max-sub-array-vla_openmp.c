d31e8f693723793e58715c53307b98a792e2b53b
Sergej Proskurin <sergejp@kth.se>
2013-11-14 01:50:18 +0100

    [src] OpenMP code needs about 0.11-0.12 sec for a 1000x1000 matrix, using all possible cores

--- src/max-sub-array-vla_openmp.c
+++ src/max-sub-array-vla_openmp.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <string.h>
+#include <omp.h>
 
 //#define _PRINT_INFO 
 
@@ -22,14 +23,48 @@ usage(const char* app_name)
     exit(0);
 }
 
+/* FIXME - do we need parallelism here? is equally fast */
 void 
 clear(
 	int* a, 
 	int len) 
 {
-    for (int index=0; index<len; index++) {
-        *(a+index) = 0;
-    }
+#if STOP_TIME
+//	double start, time;
+//	start = omp_get_wtime();
+#endif /* STOP_TIME */
+
+	/* precompute vertical prefix sum */
+
+#if OMP
+	/* FIXME - does not make sense to parallelize??? */
+//	#pragma omp parallel for \
+//		if(len > 512) \
+//		schedule(static) \
+//		num_threads(4)
+#endif
+#if 0
+    for (int index=0; index<len; index+=4) {
+		/* use loop unrolling */
+		int i,j,k,l;
+		
+		i=index;
+		j=index+1;
+		k=index+2;
+		l=index+3;
+		
+		a[i] = 0;
+		a[j] = 0;
+		a[k] = 0;
+		a[l] = 0;
+	}
+#endif
+memset((void*)a, 0, len); 
+
+#if STOP_TIME
+//	time = omp_get_wtime() - start;
+//	printf("[TIME] - clear(): time needed %f sec\n", time); 
+#endif /* STOP_TIME */
 }
 
 int** 
@@ -121,13 +156,18 @@ precomp_matrix(
 	int **ps, 
 	int dim) 
 {
-	double start, end;
-
+#if STOP_TIME
+	double start, time;
 	start = omp_get_wtime();
+#endif /* STOP_TIME */
+
 	/* precompute vertical prefix sum */
+#if OMP
 	#pragma omp parallel for \
-		schedule(static) \
-		num_threads(thread_nr) 
+		if(dim > 8) \
+		schedule(static)  
+//		num_threads(thread_nr)
+#endif
 	for(int j=0; j<dim; j++) {
         ps[0][j] = mat[0][j];
         for (int i=1; i<dim; i++) {
@@ -135,9 +175,10 @@ precomp_matrix(
         }
     }
 
-	end = omp_get_wtime() - start;
-
-	printf("[DEBUG]
+#if STOP_TIME
+	time = omp_get_wtime() - start;
+	printf("[TIME] - precomp_matrix(): time needed %f sec\n", time); 
+#endif /* STOP_TIME */
 }
 
 void 
@@ -167,8 +208,15 @@ max_sub_arr(
 	print_matrix(ps, dim, dim); 
 #endif
 
-    for (int i=0; i<dim; i++) {
-        for (int k=i; k<dim; k++) {
+    for(int i=0; i<dim; i++) {
+#if OMP
+		#pragma omp parallel for \
+			schedule(static) \
+			private(sum, pos, local_max) \
+			shared(max_sum, top, bottom, left, right)
+//			num_threads(8)
+#endif
+		for(int k=i; k<dim; k++) {
             /* Kandane over all columns with the i..k rows */
             clear(sum, dim);
             clear(pos, dim);
@@ -179,8 +227,8 @@ max_sub_arr(
              * -> Notice that we do not keep track of the max value, 
 			 *    but only its position */
             sum[0] = ps[k][0] - (i==0 ? 0 : ps[i-1][0]);
-            for (int j=1; j<dim; j++) {
-                if (sum[j-1] > 0){
+            for(int j=1; j<dim; j++) {
+                if(sum[j-1] > 0){
                     sum[j] = sum[j-1] + ps[k][j] - (i==0 ? 0 : ps[i-1][j]);
                     pos[j] = pos[j-1];
                 } 
@@ -188,21 +236,27 @@ max_sub_arr(
                     sum[j] = ps[k][j] - (i==0 ? 0 : ps[i-1][j]);
                     pos[j] = j;
                 }
-                if (sum[j] > sum[local_max]){
+                if(sum[j] > sum[local_max]) {
                     local_max = j;
                 }
             } /* Kandane ends here */
-
-            if (sum[local_max] > max_sum) {
-                /* sum[local_max] is the new max value
-                 * the corresponding submatrix goes from rows i..k.
-                 * and from columns pos[local_max]..local_max */
-
-                max_sum = sum[local_max];
-                top = i;
-                left = pos[local_max];
-                bottom = k;
-                right = local_max;
+	
+			/* use flush because it is faster than critical - this way
+			 * the critical section will be accessed less often */
+			#pragma omp flush (max_sum)
+			if(sum[local_max] > max_sum) {
+				#pragma omp critical
+				if(sum[local_max] > max_sum) { 
+                	/* sum[local_max] is the new max value
+                	 * the corresponding submatrix goes from rows i..k.
+                	 * and from columns pos[local_max]..local_max */
+//printf("TEEEST hallo welt, wie geht es dir heute??? mir gehts sseeeeeehr guuuut!!! und was ist mit dir thread nur irgendwas anderes??? möchtest du mich unterbrechen???[THREAD=%d]\n", omp_get_thread_num());
+                	max_sum = sum[local_max];
+                	top = i;
+                	left = pos[local_max];
+                	bottom = k;
+                	right = local_max;
+				}
             }
         }
     }
@@ -212,13 +266,39 @@ max_sub_arr(
 
 	int outmat_row_dim = bottom - top + 1;
     int outmat_col_dim = right - left + 1;
-    outmat = alloc_matrix(outmat_row_dim, outmat_col_dim);
+//	int xoutmat[outmat_row_dim][outmat_col_dim];
+	outmat = alloc_matrix(outmat_row_dim, outmat_col_dim);
 
+#if 0
+int k=0;
+	#pragma omp parallel for \
+		schedule(static) \
+		firstprivate(k)
+//		num_threads(thread_nr)
+    for(int i=top/*, k=0*/; i<=bottom; i++/*, k++*/) {
+        for(int j=left, l=0; j<=right; j++, l++) {
+            outmat[k][l] = mat[i][j];
+        }
+		k++;
+    }
+int l=0;
+    for(int i=top, k=0; i<=bottom; i++, k++) {
+		#pragma omp parallel for \
+			schedule(static) \
+			firstprivate(l)
+//			num_threads(thread_nr)
+        for(int j=left/*, l=0*/; j<=right; j++/*, l++*/) {
+            outmat[k][l] = mat[i][j];
+			l++;
+        }
+	}
+#endif
+	/* TODO - Question: how to parallelize??? maybe only a small nr of threads? */
     for(int i=top, k=0; i<=bottom; i++, k++) {
         for(int j=left, l=0; j<=right; j++, l++) {
             outmat[k][l] = mat[i][j];
         }
-    }
+	}
 
     alg_end = get_usecs();
 
@@ -237,14 +317,14 @@ max_sub_arr(
 			((double)(alg_end-alg_start))/1000000);
 	
 	printf("[DEBUG] freeing outmat\n");
-	free_matrix(outmat, outmat_row_dim); 
+//	free_matrix(outmat, outmat_row_dim); 
 }
 
 int 
 main(int argc, char* argv[]) 
 {
 	int dim = 0;
-	int **mat, **ps, **outmat;
+	int **mat=NULL, **ps=NULL, **outmat=NULL;
 	FILE* input_file;
     
 	if(argc != 2) {
