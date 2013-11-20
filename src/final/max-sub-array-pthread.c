48f4e5b0ca0c07242baff14f9893376761d0bca2
3lim <elim@gulli.com>
2013-11-20 20:05:09 +0100

    Implemented Framework for pthread

--- src/final/max-sub-array-pthread.c
+++ src/final/max-sub-array-pthread.c
@@ -128,11 +128,74 @@ print_matrix(
     }
 }
 
-typedef struct precomp_ta
+#define get_data(type_t,t_arg) (*(type_t*)((*(thread_arg*)t_arg).data))
+#define get_start(t_arg) ((*(thread_arg*)t_arg).start)
+#define get_end(t_arg) ((*(thread_arg*)t_arg).end)
+#define get_tid(t_arg) ((*(thread_arg*)t_arg).tid)
+#define write_result(type_t,t_arg,data) (*(type_t*)((*(thread_arg*)t_arg).result)=data)
+#define divide(iterations,num_threads,thread_func,args) divide_and_reduce(iterations,num_threads,thread_func,(void*)&args,reduce_nothing,0,0)
+#define divide_reduce(iterations,num_threads,thread_func,args,reduce_func,result_out) divide_and_reduce(iterations,num_threads,thread_func,(void*)&args,reduce_func,sizeof(result_out),(void*)&result_out)
+
+void* recursive_reduce(void*(*reduce_func)(void*,void*),void* data, int n1, int n2, int element_size)
 {
-    int threadId;
+    if(n2 <= n1) return (void*)((char*)data+n1*element_size);
+    else return reduce_func(recursive_reduce(reduce_func,data,n1,n1+(n2-n1)/2,element_size),recursive_reduce(reduce_func,data,1+n1+(n2-n1)/2,n2,element_size));
+}
+
+void* reduce_nothing(void* n1,void* n2)
+{
+    return 0;
+}
+
+typedef struct
+{
+    int tid;
     int start;
     int end;
+    void* data;
+    void* result;
+} thread_arg;
+
+void divide_and_reduce(int iterations, int num_threads, void*(*thread_func)(void*), void* data, void*(*reduce_func)(void*,void*), int result_element_size, void* result_out)
+{
+    int step = iterations / num_threads;
+    int over = iterations % num_threads;
+    pthread_t threads[num_threads];
+    thread_arg thread_args[num_threads];
+    char results[result_element_size*num_threads];
+    int n=0;
+    for(int i=0; i<iterations; i+=step, n++)
+    {
+        thread_args[n].tid = n;
+        thread_args[n].data = data;
+        thread_args[n].result = (void*)(results+n*result_element_size);
+        thread_args[n].start = i;
+        if(over-->0) i++;
+        thread_args[n].end = i + step > iterations ? iterations : i + step;
+        int error = pthread_create(&threads[n],0,thread_func,(void*)&thread_args[n]);
+        if(error)
+        {
+            printf("[ERROR %d] Couldn't create thread with tid=%d,start=%d,end=%d\n",error,thread_args[n].tid,thread_args[n].start,thread_args[n].end);
+            exit(EXIT_FAILURE);
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        int error = pthread_join(threads[i],0);
+        if(error)
+        {
+            printf("[ERROR %d] Couldn't join thread with tid=%d\n",error,i);
+            exit(EXIT_FAILURE);
+        }
+    }
+    if(result_out)
+    {
+        memcpy(result_out,recursive_reduce(reduce_func,(void*)results,0,n-1,result_element_size),result_element_size);
+    }
+}
+
+typedef struct
+{
     int dim;
     int** mat;
     int** ps;
@@ -140,14 +203,12 @@ typedef struct precomp_ta
 
 void* precomp_pthread_func(void* args)
 {
-    precomp_thread_args targs = *(precomp_thread_args*)args;
+    precomp_thread_args targs = get_data(precomp_thread_args,args);
     int** mat = targs.mat;
     int** ps = targs.ps;
     int dim = targs.dim;
-    int start = targs.start;
-    int end = targs.end;
 
-    for(int j=start; j<end; j++)
+    for(int j=get_start(args); j<get_end(args); j++)
     {
         ps[0][j] = mat[0][j];
         for (int i=1; i<dim; i++)
@@ -159,125 +220,37 @@ void* precomp_pthread_func(void* args)
 
 void precomp_matrix(int **mat, int **ps, int dim, int num_threads)
 {
-    int iterations = dim;
-    int n=0;
-    int step = iterations / num_threads;
-    int over = iterations % num_threads;
-    pthread_t threads[num_threads];
-    precomp_thread_args thread_args[num_threads];
-
-    for(int i=0; i<iterations; i+=step,n++)
-    {
-        thread_args[n].threadId = n;
-        thread_args[n].dim = dim;
-        thread_args[n].start = i;
-        if(over-->0) i++;
-        thread_args[n].end = i + step > iterations ? iterations : i + step;
-        thread_args[n].ps = ps;
-        thread_args[n].mat = mat;
-
-        int error = pthread_create (&threads[n], 0, precomp_pthread_func, (void *)&thread_args[n]);
-        if(error)
-        {
-            printf("[ERROR %d] Couldn't create thread with threadId=%d,dimension=%d,start=%d,end=%d\n",error,thread_args[n].threadId,thread_args[n].dim,thread_args[n].start,thread_args[n].end);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    for(int i=0; i<n; i++)
-    {
-        int error = pthread_join(threads[i],0);
-        if(error)
-        {
-            printf("[ERROR %d] Couldn't join thread with threadId=%d\n",error,i);
-            exit(EXIT_FAILURE);
-        }
-    }
+    precomp_thread_args args;
+    args.dim = dim;
+    args.mat = mat;
+    args.ps = ps;
+    divide(dim,num_threads,precomp_pthread_func,args);
 }
 
-typedef struct tr
+typedef struct
 {
     int max_sum;
     int left;
     int right;
     int top;
     int bottom;
-} thread_ret;
+} maxarray_thread_ret;
 
-typedef struct ta
+typedef struct
 {
-    int threadId;
     int dimension;
-    int start;
-    int end;
     int** ps;
     int* iter_i;
     int mat0;
-    thread_ret result;
-} thread_args;
-
-
-thread_ret maxArray_pthread(int** ps, int** mat, int dim, int num_threads);
-void* maxArray_pthread_func(void* tdata);
-
-thread_ret maxArray_pthread(int** ps, int** mat, int dim, int num_threads)
-{
-    int iterations = dim * (dim + 1) / 2;
-
-    int iter_i[dim];
-    int sum = 0;
-    for(int i=0; i<dim; sum+=dim-i,i++) iter_i[i] = sum;
-
-    pthread_t threads[num_threads];
-    thread_args thread_args[num_threads];
-    int n=0;
-    int step = iterations / num_threads + 1;
-    for(int i=0; i<iterations; i+=step,n++)
-    {
-        thread_args[n].threadId = n;
-        thread_args[n].dimension = dim;
-        thread_args[n].start = i;
-        thread_args[n].end = i + step > iterations ? iterations : i + step;
-        thread_args[n].iter_i = iter_i;
-        thread_args[n].ps = ps;
-        thread_args[n].mat0 = mat[0][0];
-
-        int error = pthread_create (&threads[n], 0, maxArray_pthread_func, (void *)&thread_args[n]);
-        if(error)
-        {
-            printf("[ERROR %d] Couldn't create thread with threadId=%d,dimension=%d,start=%d,end=%d\n",error,thread_args[n].threadId,thread_args[n].dimension,thread_args[n].start,thread_args[n].end);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    thread_ret ret;
-    ret.max_sum = mat[0][0];
-    ret.top = 0;
-    ret.left = 0;
-    ret.bottom = 0;
-    ret.right = 0;
-    for(int i=0; i<n; i++)
-    {
-        int error = pthread_join(threads[i],0);
-        if(error)
-        {
-            printf("[ERROR %d] Couldn't join thread with threadId=%d\n",error,i);
-            exit(EXIT_FAILURE);
-        }
-
-        if(thread_args[i].result.max_sum > ret.max_sum) ret = thread_args[i].result;
-    }
-
-    return ret;
-}
+} maxarray_thread_args;
 
 void* maxArray_pthread_func(void* tdata)
 {
-    thread_args targs = *(thread_args*)tdata;
-    int tid = targs.threadId;
+    maxarray_thread_args targs = get_data(maxarray_thread_args,tdata);
+    int tid = get_tid(tdata);
     int dim = targs.dimension;
-    int start = targs.start;
-    int end = targs.end;
+    int start = get_start(tdata);
+    int end = get_end(tdata);
     int* iter_i = targs.iter_i;
     int** ps = targs.ps;
 
@@ -343,16 +316,37 @@ void* maxArray_pthread_func(void* tdata)
         }
     }
 
-    thread_ret ret;
+    maxarray_thread_ret ret;
     ret.max_sum = max_sum;
     ret.top = top;
     ret.left = left;
     ret.bottom = bottom;
     ret.right = right;
 
-    ((thread_args*)tdata)->result = ret;
+    write_result(maxarray_thread_ret,tdata,ret);
+}
 
-    return 0;
+void* reduce_maxarray(void* d1, void* d2)
+{
+    if((*(maxarray_thread_ret*)d1).max_sum > (*(maxarray_thread_ret*)d2).max_sum) return d1;
+    return d2;
+}
+
+maxarray_thread_ret maxarray_pthread(int** ps, int** mat, int dim, int num_threads)
+{
+    maxarray_thread_args args;
+
+    int iter_i[dim];
+    for(int i=0, sum=0; i<dim; sum+=dim-i,i++) iter_i[i] = sum;
+
+    args.iter_i = iter_i;
+    args.dimension = dim;
+    args.ps = ps;
+    args.mat0 = mat[0][0];
+
+    maxarray_thread_ret ret;
+    divide_reduce(dim * (dim + 1) / 2,num_threads,maxArray_pthread_func,args,reduce_maxarray,ret);
+    return ret;
 }
 
 int
@@ -389,7 +383,7 @@ main(int argc, char* argv[])
 
         precomp_matrix(mat, ps, dim, NUM_THREADS);
 
-        thread_ret ret = maxArray_pthread(ps, mat, dim, NUM_THREADS);
+        maxarray_thread_ret ret = maxarray_pthread(ps, mat, dim, NUM_THREADS);
         printf("%d %d %d %d\n",ret.left,ret.top,ret.right,ret.bottom);
 
 
